981ab233e7255575b8c5c881e4c2472f8bc8b97a
Vel Murugan <mvel3987@gmail.com>
2025-06-03 11:03:29 +0530

    Version 1

--- sample.java
+++ sample.java
@@ -1,6 +1,16 @@
+class calc{
+    public long add (long N1 , long N2){
+        return N1 + N2;
+    }
+    public long sub (long N1 , long N2){
+        return N1 - N2;
+    }
+}
 public class sample {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("git pull!");
+        calc op = new calc();
+        System.out.println("addition: " + op.add(29,11));
+        System.out.println("Subtraction: " + op.sub(29,11));
     }
 }
